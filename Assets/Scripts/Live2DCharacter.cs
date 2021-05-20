49ed41b42c1143b50f0bf4448927ddbb4d518d15
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-21 00:54:33 +0900

    とりあえずウインクさせてみるテスト

--- Assets/Scripts/Live2DCharacter.cs
+++ Assets/Scripts/Live2DCharacter.cs
@@ -1,10 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Live2D.Cubism.Core;
+using UniRx;
+using System;
+using UniRx.Operators;
 
 /// <summary>
 /// Live2Dのキャラクタ
 /// </summary>
 public class Live2DCharacter : MonoBehaviour
 {
+    /// <summary>
+    /// 左目の開き具合
+    /// </summary>
+    [SerializeField]
+    private CubismParameter LeftEyeOpen = null;
+
+    /// <summary>
+    /// 右目の開き具合
+    /// </summary>
+    [SerializeField]
+    private CubismParameter RightEyeOpen = null;
+
+    void Awake()
+    {
+        // これでウインクになるはず
+        LeftEyeOpen.Value = 0.0f;
+        RightEyeOpen.Value = 1.0f;
+    }
 }
