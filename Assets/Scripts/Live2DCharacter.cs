bdd7569460fbb2123462374bf86b557e745d2b9c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-21 00:39:35 +0900

    キャラクタ制御用のクラスをとりあえず定義

---
+++ Assets/Scripts/Live2DCharacter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Live2Dのキャラクタ
+/// </summary>
+public class Live2DCharacter : MonoBehaviour
+{
+}
