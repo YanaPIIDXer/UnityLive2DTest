04001f60bea3475997d8ee16e4134c4c16857bfd
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-21 10:41:02 +0900

    キー入力用クラスをとりあえず定義

---
+++ Assets/Scripts/KeyInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+/// <summary>
+/// キー入力
+/// </summary>
+public class KeyInput : MonoBehaviour
+{
+}
