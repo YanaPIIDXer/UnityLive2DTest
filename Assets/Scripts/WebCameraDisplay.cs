a6fed0e7c1cbfb0441dd0c66b71eeb66bb5b1b96
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-22 04:56:31 +0900

    カメラの映像を流せるようにした

---
+++ Assets/Scripts/WebCameraDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Webカメラ表示
+/// </summary>
+public class WebCameraDisplay : MonoBehaviour
+{
+    void Awake()
+    {
+        var Device = WebCamTexture.devices[0];
+        var Tex = new WebCamTexture(Device.name, 1920, 1080, 60);
+        var TextureRenderer = GetComponent<Renderer>();
+        TextureRenderer.material.mainTexture = Tex;
+        Tex.Play();
+    }
+}
