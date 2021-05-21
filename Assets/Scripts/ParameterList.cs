6a2de444e11d37578b2c932393447e87c8fd64e4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-21 09:57:34 +0900

    謎のコンフリクト解消

--- Assets/Scripts/ParameterList.cs
+++ Assets/Scripts/ParameterList.cs
@@ -1,11 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Live2D.Cubism.Core;
 
 /// <summary>
 /// パラメータをリスト構造で管理するクラス
 /// </summary>
 public class ParameterList
 {
+    /// <summary>
+    /// パラメータリスト
+    /// </summary>
+    private List<CubismParameter> Params = new List<CubismParameter>();
 
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public ParameterList()
+    {
+    }
+
+    /// <summary>
+    /// 追加
+    /// </summary>
+    /// <param name="Param">パラメータ</param>
+    public void Add(CubismParameter Param)
+    {
+        Params.Add(Param);
+    }
+
+    /// <summary>
+    /// 値を設定
+    /// </summary>
+    /// <param name="Value">価</param>
+    /// <param name="Offset">パラメータとパラメータの間に設けるオフセット値</param>
+    public void SetValue(float Value, float Offset = 0.0f)
+    {
+        float AddValue = Value;
+        foreach (var Param in Params)
+        {
+            Param.Value = AddValue;
+            AddValue += Offset;
+        }
+    }
 }
